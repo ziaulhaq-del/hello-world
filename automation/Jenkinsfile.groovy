ef6e3654c46049f873ce29afbf052a1273101cbd
MohamedHamdy404 <mohamedh4mdy1997@gmail.com>
2024-04-24 15:52:12 +0200

    change

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -1,38 +1,32 @@
-pipeline{
-    
+pipeline {
     agent any;
 
     environment {
-        
         GITHUB_REPO = "MohamedHamdy404/devops"
         DOCKER_REGISTRY = ""
         DOCKER_IMAGE = ""
         imageTag = ""
         imageName = ""
-        
     }
 
-    stages{
-
-        stage ("checkout"){
-
+    stages {
+        stage("checkout") {
             steps {
                 git url: "https://github.com/${GITHUB_REPO}.git", branch: 'develop'
                 sh 'cat flag.txt'
             }
         }
-        stage ("tagging"){
-            steps {
-                    script{
-                        sh ' echo "autotag started" '
-                        sh "git config --global --add safe.directory ${env.WORKSPACE}"
 
-                        pipelineScripts = load "automation/tagging.groovy"
-                        pipelineScripts.AutoTag;
-                    }
+        stage("tagging") {
+            steps {
+                script {
+                    sh 'echo "autotag started"'
+                    sh "git config --global --add safe.directory ${env.WORKSPACE}"
+
+                    def pipelineScripts = load "tagging.groovy" // Corrected path
+                    pipelineScripts.AutoTag();
                 }
             }
+        }
     }
-
-//test
 }
