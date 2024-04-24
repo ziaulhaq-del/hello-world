ad3f3ac705e3826f5374d158ca50f86404a928c0
MohamedHamdy404 <mohamedh4mdy1997@gmail.com>
2024-04-24 15:17:53 +0200

    change

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -1,13 +1,6 @@
 pipeline{
     
-    agent {
-    kubernetes {
-			// label 'build-pod'  // all your pods will be named with this prefix, followed by a unique id
-			// idleMinutes 30  // how long the pod will live after no jobs have run on it
-			 yamlFile 'automation/bin/build-pod-maven.yaml'  // path to the pod definition relative to the root of our project 
-			// defaultContainer 'maven'  // define a default container if more than a few stages use it, will default to jnlp container
-    	}
-    }
+    agent any;
 
     environment {
         
