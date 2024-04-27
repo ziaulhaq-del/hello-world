a4073d50120c37036750997d6579d459065de4c8
MohamedHamdy404 <mohamedh4mdy1997@gmail.com>
2024-04-27 18:04:03 +0300

    change

--- automation/tag.groovy
+++ automation/tag.groovy
@@ -5,11 +5,11 @@ def AutoTag() {
         env.STREAM_VERSION = sh ( script: 'git describe --abbrev=0 --tags --match=$MY_BRANCH*', returnStdout: true).trim()      //Store Full tag
         env.STREAM = sh ( script: 'echo $STREAM_VERSION | cut -d "-" -f1', returnStdout: true).trim()       //Store Stream Name
         env.CURRENT_MAX = sh ( script: 'echo $STREAM_VERSION | cut -d "-" -f2 ', returnStdout: true).trim() //Store Main Max 
-        sh'echo we are at ${env.MY_BRANCH} stage'   //Print Stage
+        sh'echo we are at ${MY_BRANCH} stage'   //Print Stage
         sh 'echo ${STREAM_VERSION}'     //Print Latest Tag to the stage
         sh 'echo ${STREAM}'     //Print Stream Name
         sh 'echo ${CURRENT_MAX}'    //Print Main Max
-        sh 'echo this is old ${env.MY_BRANCH} version ${LINE_VERSION}'      //Print old version
+        sh 'echo this is old ${MY_BRANCH} version ${LINE_VERSION}'      //Print old version
         // Switch cases based on branch name 
        
     }
