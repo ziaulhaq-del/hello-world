1e7e02950fa2715e8db840a4bd8e4a7252ac9145
MohamedHamdy404 <mohamedh4mdy1997@gmail.com>
2024-04-28 09:25:45 +0300

    testing release tagging

---
+++ automation/Jenkinsfile.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any;
+
+    environment {
+        GITHUB_REPO = "MohamedHamdy404/devops"
+        DOCKER_REGISTRY = ""
+        DOCKER_IMAGE = ""
+        imageTag = ""
+        imageName = ""
+        MY_BRANCH = sh(script: 'git rev-parse --abbrev-ref HEAD', returnStdout: true).trim()
+    }
+
+    stages {
+        stage("checkout") {
+            steps {
+                git url: "https://github.com/${GITHUB_REPO}.git", branch: 'develop'
+                sh 'cat flag.txt'
+            }
+        }
+
+        stage("tagging") {
+            steps {
+                script {
+                    sh 'echo "autotag started"'
+                    pipelineScripts = load "automation/tag.groovy"
+					pipelineScripts.AutoTag()
+                    
+                }
+            }
+        }
+    }
+}
