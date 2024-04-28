590b56bd048f6384ff3545299d52805f9ecf7568
MohamedHamdy404 <mohamedh4mdy1997@gmail.com>
2024-04-28 12:14:16 +0300

    testing tags

--- automation/tag.groovy
+++ automation/tag.groovy
@@ -3,8 +3,6 @@ def AutoTag() {
         sh 'echo TAG IS STARTED '
         sh 'echo ${MY_BRANCH}'
         env.STREAM_VERSION = sh ( script: 'git describe --abbrev=0 --tags --match=$MY_BRANCH*', returnStdout: true).trim()      //Store Full tag
-        
-        /*
         env.STREAM = sh ( script: 'echo $STREAM_VERSION | cut -d "-" -f1', returnStdout: true).trim()       //Store Stream Name
         env.CURRENT_MAX = sh ( script: 'echo $STREAM_VERSION | cut -d "-" -f2 ', returnStdout: true).trim() //Store Main Max 
         env.LINE_VERSION = sh ( script: 'echo $STREAM_VERSION | cut -d "-" -f4', returnStdout: true).trim()
@@ -17,7 +15,7 @@ def AutoTag() {
         sh 'echo ${LINE_VERSION}'
         sh 'echo this is old ${MY_BRANCH} version ${LINE_VERSION}'      //Print old version
         // Switch cases based on branch name 
-       */
+       
     }
 }
 return this;
