3fd9539d2bb656e67c4eda051791b8acad2fed44
MohamedHamdy404 <mohamedh4mdy1997@gmail.com>
2024-04-29 14:40:39 +0300

    updating automation folder

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -59,7 +59,7 @@ pipeline {
                     def jsonContent = readFile('envi.json')
                     def envi = new groovy.json.JsonSlurper().parseText(jsonContent)
                     sh ' echo "LOADED JSON"'
-                    env.PROJECT_URL = envi.services.service[0].PROJECT_UR
+                    env.PROJECT_URL = envi.services.service[1].PROJECT_UR
                     sh 'echo ${PROJECT_URL}'
                     
                 }
