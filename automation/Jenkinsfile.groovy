05c86fb2685a2395a4e67c033017e0fc2774a40e
= <=>
2024-05-02 11:37:04 +0300

    dddd

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     environment {
 
         PROJECT_KEY= "${env.SERVICE_NAME}-${MY_BRANCH}"
-        SONAR_TOKEN: credentials('JENKINS_SONAR_TOKEN_USER')
+        SONAR_TOKEN= credentials('JENKINS_SONAR_TOKEN_USER')
         CONFIG_FILE = 'automation/generic_config.yaml'  
         ENV_VARS_FILE = 'automation/environment_vars.yaml'
         
