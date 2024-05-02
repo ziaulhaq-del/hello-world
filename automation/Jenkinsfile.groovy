3a246ad586d354c063b6ff4b35e7c3b6e80c0330
= <=>
2024-05-02 16:58:50 +0300

    gg

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -1,3 +1,5 @@
+
+def user = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)?.userName ?: "Unknown"
 pipeline {
     agent any;
 
@@ -14,7 +16,7 @@ pipeline {
 			<p> Jenkins Job Console Log:   <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>
 			<p> New Tag Version: [${env.TAG}] </p>
 			"""
-        def user = "${currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)?.userName}"
+        
     }
 
     stages {
