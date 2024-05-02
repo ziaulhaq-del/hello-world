f1aa3cb244c131b8456eaa54db9281c6018e9c82
= <=>
2024-05-02 14:01:12 +0300

    fff

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -76,8 +76,8 @@ pipeline {
                 
                 echo "Project URL for microservice_1: ${env.develop_NAMESPACE}"
 
-                SERVICE_NAME = ${env.pipelinedemo_SERVICE_NAME}
-                MY_BRANCH = ${env.develop_MY_BRANCH}
+                env.SERVICE_NAME = ${env.pipelinedemo_SERVICE_NAME}
+                env.MY_BRANCH = ${env.develop_MY_BRANCH}
                 echo "Micro_1 Project Key for microservice_1: ${env.pipelinedemo_REPOSITORY}"
 
 
