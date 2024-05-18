634b19f06c591a378e1e3866f182b9c0348eaa9c
= <=>
2024-05-18 17:36:25 +0300

    test side script

--- automation/Jenkinsfile.groovy
+++ automation/Jenkinsfile.groovy
@@ -3,9 +3,9 @@ pipeline {
 
     environment {
         SONAR_TOKEN= credentials('JENKINS_SONAR_TOKEN_USER')
-        CONFIG_FILE = 'automation/generic_config.yaml'  
-        ENV_VARS_FILE = 'automation/environment_vars.yaml'
-        GITHUB_REPO = "MohamedHamdy404/devops"
+        //CONFIG_FILE = 'automation/generic_config.yaml'  
+       // ENV_VARS_FILE = 'automation/environment_vars.yaml'
+        //GITHUB_REPO = "MohamedHamdy404/devops"
         status = "green"
        // currentBuildCommitHash = "55555"
 
@@ -94,30 +94,10 @@ pipeline {
                     
                     sh "git config --global --add safe.directory ${env.WORKSPACE}"
                     
-                    def currentBuildCommitHash = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
-                    echo "Commit hash of the current build: ${currentBuildCommitHash}"
-                    // Fetch the previous build information
-                    def previousBuildInfo = currentBuild.rawBuild.getPreviousSuccessfulBuild()
-                    if (previousBuildInfo) {
-                        // Extract the commit hash from the previous build
-                        def previousBuildCommitHash = previousBuildInfo.actions.find { it instanceof hudson.plugins.git.util.BuildData }.lastBuiltRevision.sha1String
-                        echo "Commit hash of the previous successful build: ${previousBuildCommitHash}"
-                        
-                        // Get the commit hash of the current build
-
-                        
-                        // Compare the commit hashes
-                        if (currentBuildCommitHash == previousBuildCommitHash) {
-                            echo "The commit hashes match. No changes since the previous successful build."
-                            status = "red"
-                        } else {
-                            echo "The commit hashes are different. Changes detected since the previous successful build."
-                        }
-                    } else {
-                        echo "No previous successful build found."
-                    }
+					pipelineScripts = load "automation/changesDiff.groovy"
+					pipelineScripts.CompileCode()
+				    }
                     
-
                     //env.PROJECT_URL = envi.services.service[1].PROJECT_UR
                     //echo "Jenkins server URL for microservice_2: ${env.jenkins_server_url}"
                    
---
+++ automation/changeDiff.groovy
@@ -0,0 +1,27 @@
+def getChanges() {
+     def currentBuildCommitHash = sh(script: 'git rev-parse HEAD', returnStdout: true).trim()
+        echo "Commit hash of the current build: ${currentBuildCommitHash}"
+        // Fetch the previous build information
+        def previousBuildInfo = currentBuild.rawBuild.getPreviousSuccessfulBuild()
+        if (previousBuildInfo) {
+            // Extract the commit hash from the previous build
+            def previousBuildCommitHash = previousBuildInfo.actions.find { it instanceof hudson.plugins.git.util.BuildData }.lastBuiltRevision.sha1String
+            echo "Commit hash of the previous successful build: ${previousBuildCommitHash}"
+            
+            // Get the commit hash of the current build
+
+            
+            // Compare the commit hashes
+            if (currentBuildCommitHash == previousBuildCommitHash) {
+                echo "The commit hashes match. No changes since the previous successful build."
+                env.flag = "red"
+            } else {
+                echo "The commit hashes are different. Changes detected since the previous successful build."
+            }
+        } else {
+            echo "No previous successful build found."
+        }
+                    
+
+}
+return this;
